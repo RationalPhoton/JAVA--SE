ae078b272500ed3051fae73676799439cb459fcb
Shashikant Jagtap <sbj8612@gmail.com>
2022-04-18 16:40:57 +0530

    Adding Hello world Java Program

---
+++ MyFirst.java
@@ -0,0 +1,11 @@
+//Problem statement : Write your first Java Program to say Hello to world. i.e. Print Hello World on console.
+
+import java.lang.*;
+
+class MyFirst
+{
+	public static void main(String arg[])
+	{
+		System.out.println("Hello World!");
+	}
+}
